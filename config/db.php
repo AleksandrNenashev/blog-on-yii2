62b27f1b7c0353bcb67d5c881f6bd607bddb3fdb
Aleksandr Nenashev <nenashev88@gmail.com>
2021-07-13 18:02:35 +0400

    Create migrations

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=yiinew',
+    'username' => 'yiinew',
+    'password' => 'IS3X2KTKfaYAZBHz',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
